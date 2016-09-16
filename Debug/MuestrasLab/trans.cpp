5e5c6dd68c854a8d6a9337146e50f0273c450c06
rjmantilla76 <rj.mantilla76@uniandes.edu.co>
2016-09-16 18:13:43 -0500

    Images Added

---
+++ Debug/MuestrasLab/trans.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+main(){
+    freopen("PrimeraMuestraSceneFrame.map","r",stdin);
+    freopen("PrimeraMuestraFrame.map","w",stdout);
+    string a;
+    int b,c;
+    cin>>a;
+    cout<<a<<endl;
+    cin>>a;
+    cout<<a<<endl;
+    while(cin>>b>>c){
+        cout<<b-450<<" "<<c<<endl;
+    }
+}
