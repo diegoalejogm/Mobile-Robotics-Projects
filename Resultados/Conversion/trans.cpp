919f922224f88d3239078c8452854ea445905325
Diego Gomez <diegoalejogm@icloud.com>
2016-09-17 02:30:21 -0500

    Fixed project's folder architecture. Added maps with superposition of sensed data for scenario maps. Fixed some maps.

---
+++ Resultados/Conversion/trans.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>    
+using namespace std;
+main(){
+    freopen("PrimeraMuestraFrame.map","r",stdin);
+    freopen("PrimeraMuestraSceneFrame.map","w",stdout);
+    string a;
+    int b,c;
+    cin>>a;
+    cout<<a<<endl;
+    cin>>a;
+    cout<<a<<endl;
+    while(cin>>b>>c){
+        cout<<-c+450<<" "<<b+450<<endl;
+    }
+}
